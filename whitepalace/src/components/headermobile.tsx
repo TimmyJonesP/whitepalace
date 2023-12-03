632c99c80feace2d31c700ca56fcb3e22e5e4caa
Sandro <sandrocarioli43@gmail.com>
2023-12-03 18:58:08 -0300

    mobile tbf

--- whitepalace/src/components/headermobile.tsx
+++ whitepalace/src/components/headermobile.tsx
@@ -1,9 +1,90 @@
-import React from 'react'
+"use client";
 
-const headermobile = () => {
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { SIDENAV_ITEMS } from '@/constants';
+import { Icon } from '@iconify/react/dist/iconify.js';
+import { motion, useCycle } from 'framer-motion';
+
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `inset(${height * 2 + 200}px at 100% 0)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "inset(0px at 100% 0)",
+    transition: {
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
+const variants = {
+  open: {
+    transition: { staggerChilden: 0.02, delayChildren: 0.15},
+  },
+  closed: {
+    transition: { staggerChildren: 0.01, staggerDirection: -1},
+  },
+};
+const headermobile = () => 
+{
+  const pathname = usePathname();
+  const containerRef = useRef();
+  const { height } = useDimensions(containerRef);
+  const [ isOpen, toggleOpen] = useCycle(false, true);
+
   return (
-    <div>headermobile</div>
+    <motion.nav
+    initial={false}
+    animate={isOpen ? "open" : "closed"}
+    custom={height}
+    className={`fixed inset-0 z-50 w-full md:hidden ${
+      isOpen ? "" : "pointer-events-none"
+    }`}
+    ref={containerRef}
+    >
+      <motion.div
+      className="absolute inset-0 right-0 w-full bg-white"
+      variants={sidebar}
+      />
+
+      <motion.ul
+      className= "absolute grid w-full gap-3 px-10 py-16"
+      variants={variants}
+      >
+
+        {SIDENAV_ITEMS.map( (item, idx) => {
+          const isLastItem = idx === SIDENAV_ITEMS.length - 1;
+
+          return (
+          
+            )
+        })}
+        
+        </motion.ul>
+    </motion.nav>
   )
 }
 
 export default headermobile
+
+const useDimensions = (ref:any) => {
+  const dimensions = useRef({ width: 0, height: 0 });
+
+  useEffect(() => {
+    if(ref.current) {
+      dimensions.current.width = ref.current.offsetWidth;
+      dimensions.current.height = ref.current.offsetHeight;
+
+    }
+  }, [ref])
+  return dimensions.current;
+}
